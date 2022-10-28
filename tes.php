601258a67aa3aeef91b68844daf4a4830f8947c7
AlBarra26 <116324376+AlBarra26@users.noreply.github.com>
2022-10-28 18:51:28 +0700

    Add files via upload

---
+++ tes.php
@@ -0,0 +1,53 @@
+<?php
+//NO 1
+/*$bil = 80;
+if ($bil % 2 == 0) {
+    echo "$bil adalah bilangan genap<br>";
+}else{
+    echo "$bil adalah bilangan ganjil<br>";
+}
+$bil2 = 81;
+if ($bil2 % 2 == 0) {
+    echo "$bil2 adalah bilangan genap<br>";
+}else{
+    echo "$bil2 adalah bilangan ganjil<br>";
+}
+
+
+//NO 2
+$a = 6;
+$b = 10;
+if ($a > $b) {
+    echo "$a lebih besar dari $b<br>";
+}elseif ($a < $b) {
+    echo "$a lebih kecil dari $b<br>";
+}else{
+    echo "$a sama besar dengan $b<br>";
+}
+
+
+//NO 3
+$username = "admin";
+$password = "qwerty";
+if ($username == "admin" && $password == "qwerty") {
+    echo "username dan password sesuai, hak akses diberikan";
+}else{
+    echo "username dan password tidak sesuai";
+}
+
+//NO 4
+$username = "admin";
+$password = "qwerty";
+if ($username == "admin" && $password == "qwerty") {
+    echo "username dan password sesuai, hak akses diberikan";
+}elseif ($username == "admin" && $password != "qwerty") {
+    echo "username sesuai, password tidak sesuai";
+}elseif ($username != "admin" && $password == "qwerty") {
+    echo "username tidak sesuai, password sesuai";
+}elseif ($username != "admin" && $password != "qwerty") {
+    echo "username tidak sesuai, password tidak sesuai";
+}else{
+    echo "eror";
+}*/
+?>
+
